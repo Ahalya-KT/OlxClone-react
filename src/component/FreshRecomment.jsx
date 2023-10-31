98f383e326afcc8aeac91ddf145543ca96628317
Ahalya-KT <ahalyakt002@gmail.com>
2023-10-31 18:19:55 +0530

    fashion

--- src/component/FreshRecomment.jsx
+++ src/component/FreshRecomment.jsx
@@ -1,37 +1,30 @@
-import axios from 'axios'
-import React, { useState } from 'react'
-import Recoment from './Recoment.Jsx'
+import axios from "axios";
+import React, { useState } from "react";
+import Recoment from "./Recoment.Jsx";
 
 const FreshRecomment = () => {
+  const [dataItem, setdataItem] = useState();
 
-    const[dataItem,setdataItem]=useState()
-
- async function details(){
- await   axios.get("https://fakestoreapi.com/products/")
-    .then((res)=>setdataItem(res.data))
+  async function details() {
+    await axios
+      .get("https://fakestoreapi.com/products/")
+      .then((res) => setdataItem(res.data));
   }
-  details()
+  details();
 
-
-  
   return (
     <div>
-      <div className='text-xl' >
-        Fresh Recommendations
-      </div>
+      <div className="text-xl">Fresh Recommendations</div>
 
       <div>
-       {
-        dataItem && dataItem.map(li=> {
-            console.log(li)
-            return  <Recoment item={item}/>
-           
-        })
-       
-       }
+        {dataItem &&
+          dataItem.map((li) => {
+            console.log(li);
+            return <Recoment item={item} />;
+          })}
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default FreshRecomment
+export default FreshRecomment;
