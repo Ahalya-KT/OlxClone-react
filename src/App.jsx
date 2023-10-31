e2b2182e93bd955d19f43123ed30d7c243449677
Ahalya-KT <ahalyakt002@gmail.com>
2023-10-31 10:29:35 +0530

    fashion

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import Aboutus from './component/Aboutus'
 import Catgory from './component/Catgory'
 import Footer from './component/Footer'
 import Navbar from './component/Navbar'
-import Recoment from './component/Recoment.Jsx'
+// import Recoment from './component/Recoment.Jsx'
 import Review from './component/Review.jsx'
 import Home from './component/Home'
 
--- src/component/LastSearch.jsx
+++ src/component/LastSearch.jsx
@@ -1,54 +1,40 @@
-import React, { useState } from 'react'
-import {BsLightningChargeFill} from 'react-icons/bs'
-import {AiOutlineHeart} from 'react-icons/ai'
-import axios from 'axios'
-import Cards from './component/Cards.jsx'
+import React, { useState } from "react";
+import { BsLightningChargeFill } from "react-icons/bs";
+import { AiOutlineHeart } from "react-icons/ai";
+import axios from "axios";
+import Cards from "./Cards";
 
-const Cards = () => {
+const LastSearch = () => {
+  const [items, setitems] = useState([]);
 
-
-  const[items,setitems]=useState([])
-
-
- async function Carddetails(){
-
- await axios.get('https://fakestoreapi.com/products')
-  // .then(res=>res.json()) 
-   .then(res=>setitems(res.data))
-
-
- }
- Carddetails()
+  async function Carddetails() {
+    await axios
+      .get("https://fakestoreapi.com/products")
+      // .then(res=>res.json())
+      .then((res) => setitems(res.data));
+  }
+  Carddetails();
 
   return (
     <div>
+      <div className="pb-4 text-xl">Based on Your Last Search</div>
+      <div className="flex flex-wrap">
+        {items &&
+          items.map((li) => {
+            console.log(li);
+            return;
 
-        <div className='pb-4 text-xl'>
-
-        Based on Your Last Search
-
-        </div>
-  <div className='flex flex-wrap'>
-
-{items && items.map(li=>{
-        console.log(li);
-        return
-
-        <Cards/>
-        //  <div className='w-52 '>
-        //   <img  src={li.image}></img>
-        //   <p>{li.id}</p>
-        //   <p>{li.title}</p>
-        //   <p>{li.price}</p>
-        // </div>
-      })}
-</div>
-
-      
-       
-      
+            <Cards />;
+            //  <div className='w-52 '>
+            //   <img  src={li.image}></img>
+            //   <p>{li.id}</p>
+            //   <p>{li.title}</p>
+            //   <p>{li.price}</p>
+            // </div>
+          })}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Cards
+export default LastSearch;
